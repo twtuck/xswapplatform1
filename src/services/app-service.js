936c0cdd4b1d406ed93f2977656f26625ba192e3
michelle-phan <phannguyentramy@gmail.com>
2019-10-23 02:59:44 +0800

    test 5

--- src/services/app-service.js
+++ src/services/app-service.js
@@ -2,8 +2,7 @@
 //const { axios } = require("axios");
 import axios from 'axios';
 
-const baseApiUrl = 'https://my-json-server.typicode.com/michelle-phan/fakeAPIs/';
-const baseApiUrl1 = 'https://m876ampgkh.execute-api.ap-southeast-1.amazonaws.com/dev';
+const baseApiUrl = 'https://m876ampgkh.execute-api.ap-southeast-1.amazonaws.com/dev';
 
 
 // add app
@@ -14,19 +13,19 @@ export const addApp = (app, token) => {
         const { name, description, company, facebookClientId, facebookClientSecret } = app;
 
         axios
-            .post(`${baseApiUrl1}/provisions`, null,
+            .post(`${baseApiUrl}/provisions`, null,
             { headers: { Authorization: `Bearer ${token}`} ,
               data: {
                 appName: name,
                 appInfo: {
                     description: description,
-                    company: "ABC Company"
+                    company: company //"ABC Company"
                 },
                 callbackUrl: "https://enjdkyfy0odvk.x.pipedream.net",
                 logoutUrl: "https://enjdkyfy0odvk.x.pipedream.net",
                 facebookClient: {
-                    client_id: "466080827334160",
-                    client_secret: "a475c57454a898495a0187b11a3096fd"
+                    client_id: facebookClientId, //"466080827334160",
+                    client_secret: facebookClientSecret //"a475c57454a898495a0187b11a3096fd"
     }
             } })
             .then((result) => {
@@ -82,7 +81,7 @@ export const listApps = (token) => {
 
     return new Promise((resolve, reject) => {
         axios
-            .get(`${baseApiUrl1}/provisions`, { headers: { Authorization: `Bearer ${token}`} })
+            .get(`${baseApiUrl}/provisions`, { headers: { Authorization: `Bearer ${token}`} })
             .then((result) => {
                 resolve(result.data.Items);
                 return;
