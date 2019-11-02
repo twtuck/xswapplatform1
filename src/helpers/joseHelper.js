4cf387606ee79cd3a865feb8c20af33fd249ac34
waituck <wai_tuck@yahoo.com>
2019-11-02 22:39:14 +0800

    Revert "Replaces node-jose with jose"
    This reverts commit de3db96f46a134dfd241fced01fe16f5171e41bd.

--- src/helpers/joseHelper.js
+++ src/helpers/joseHelper.js
@@ -1,26 +1,25 @@
-const jose = require('jose');
+const jose = require('node-jose');
 
-async function encrypt(publicKeyPem, plainText) {
+export const encrypt = async (publicKeyPem, plainText) => {
     if (typeof plainText !== 'string')
         throw new Error(`input 'plainText' must be a string`);
 
-    const publicKeyJwk = await jose.JWK.asKey({
-        key: publicKeyPem, 
-        format: 'pem'
-    });
-    return await jose.JWE.encrypt(plainText, publicKeyJwk);
+    const publicKeyJwk = await jose.JWK.asKey(publicKeyPem, 'pem');
+    return await jose.JWE.createEncrypt({
+            format: 'compact'
+        }, publicKeyJwk)
+        .update(plainText)
+        .final();
     // return jwe
 }
 
-async function decrypt(privateKeyPem, jwe) {
+export const decrypt = async (privateKeyPem, jwe) => {
     if (typeof jwe !== 'string')
         throw new Error(`input 'jwe' must be a string`);
 
-    const privateKeyJwk = await jose.JWK.asKey({
-        key: privateKeyPem, 
-        format: 'pem'
-    });
-    const output = await jose.JWE.decrypt(jwe, privateKeyJwk);
-    return output.toString();
+    const privateKeyJwk = await jose.JWK.asKey(privateKeyPem, 'pem');
+    const decryptedContent = await jose.JWE.createDecrypt(privateKeyJwk)
+        .decrypt(jwe);
+    return decryptedContent.plaintext.toString();
     // return plain text
 }
